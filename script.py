9eedc89f8383e86d6515a4c9aa1b381369eab42e
FRDevelop <farhad.develop@gmail.com>
2020-02-14 04:50:05 +0400

    update

--- script.py
+++ script.py
@@ -1,18 +1,1 @@
-class Family(object):
-    def __init__(self, name, surname):
-        self.__name = name
-        self.__surname = surname
-    
-    @property
-    def name(self):
-        return self.__name
-    
-    @name.setter
-    def name(self, name):
-        assert isinstance(name, str)
-        self.__name = name
-    
-farhad = Family(63, 'Maharramov')
-
-print(farhad.name)
-print(farhad.name)
+print(int('6.0'))
