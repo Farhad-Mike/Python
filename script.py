e0d223f45e5bf762558d91d202b9bfc13abcf2c6
FRDevelop <farhad.develop@gmail.com>
2020-02-16 20:48:23 +0400

    update

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
-print(int('6.0'))
+import xml.dom.minidom
+
+dom = xml.dom.minidom.parse('xml')
+element = dom.getElementsByTagName('incident')
+print(element[0].childNodes[0].data)
+print(element[0].childNodes[0].nodeType)
