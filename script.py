aff8767503240818a96c2aa550034bf9b15fa8cb
Farhad-Mike <54776279+Farhad-Mike@users.noreply.github.com>
2022-06-27 12:29:13 +0400

    i

--- script.py
+++ script.py
@@ -1,14 +1,13 @@
-import numpy
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
 
-# Может делать арифметические вычисления последовательно с каждым элементов списка
-height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
-weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
-np_height = numpy.array(height)
-np_weight = numpy.array(weight)
-bmi = np_weight / np_height ** 2
+from numpy import array_equiv
+import pandas as pd 
+brics = pd.DataFrame(dict)
+brics.index = [1, 2, 3, 4, 5]
 
-# For a boolean response
-print(bmi > 23)
+area = brics[['country', 'capital']]
 
-# Print only those observations above 23
-print(bmi[bmi > 23])
+print(brics[1:3])
